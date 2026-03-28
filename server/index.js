c88d8c56ff7e37857a4c9190747f59151a34cb5a
ABDUL JAFAROV <c.ebdul@gmail.com>
2026-03-29 01:10:20 +0300

    fix: set default terminal cwd to /app

--- server/index.js
+++ server/index.js
@@ -207,7 +207,7 @@ io.on('connection', (socket) => {
         name: 'xterm-256color',
         cols: 80,
         rows: 24,
-        cwd: process.env.HOME || '/root',
+        cwd: '/app',
         env
       });
 
